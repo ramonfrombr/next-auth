1cead610329556f10336e1cf5e32f69361e62364
Ramon Rodrigues <ramon.rodrigues@mouts.info>
2024-09-07 18:32:07 -0300

    Implemented reset password page

---
+++ app/auth/new-password/page.tsx
@@ -0,0 +1,12 @@
+import { NewPasswordForm } from "@/components/auth/new-password-form";
+import React from "react";
+
+const NewPasswordPage = () => {
+  return (
+    <div>
+      <NewPasswordForm />
+    </div>
+  );
+};
+
+export default NewPasswordPage;
