8f580d11e0bae92755102cf3516e29aaeb13e02c
Ramon Rodrigues <ramonfrombr@gmail.com>
2024-09-29 11:32:18 -0300

    Created userButton componente

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,22 +1,19 @@
+"use client";
+import { logout } from "@/actions/logout";
+import { useCurrentUser } from "@/hooks/use-current-user";
 import React from "react";
 
-import { auth, signOut } from "@/auth";
+const SettingsPage = () => {
+  const user = useCurrentUser();
+  const onClick = () => {
+    logout();
+  };
 
-const SettingsPage = async () => {
-  const session = await auth();
   return (
-    <div>
-      {JSON.stringify(session)}
-
-      <form
-        action={async () => {
-          "use server";
-
-          await signOut({ redirectTo: "/auth/login" });
-        }}
-      >
-        <button type="submit">Sign out</button>
-      </form>
+    <div className="bg-white p-10 rounded-xl">
+      <button onClick={onClick} type="submit">
+        Sign out
+      </button>
     </div>
   );
 };
